eb5bde524dfe8912b0134b9a4fe57ff967bf3b8b
Chloe <b.bernard@lyon.ort.asso.fr>
2023-06-12 16:01:37 +0200

    commit nav sur écran (enDev)

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.homeScreen}>
+      <Text>HomeScreen</Text>
+      <Button
+        title="Go to details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
 
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={styles.homeScreen}>
+      <Text>DetailsScreen</Text>
+      <Button
+        title="Go to details ... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar style="auto" />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
+  homeScreen: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
